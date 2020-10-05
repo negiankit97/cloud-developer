1dad6b8ab49dc96197270f5533432aed5cb5ad0c
Ankit Negi <ankit.negi@calsoftinc.com>
2020-10-05 16:02:44 +0530

    Initial commit

--- course-02/exercises/udacity-c2-basic-server/src/unit-test-examples/units.ts
+++ course-02/exercises/udacity-c2-basic-server/src/unit-test-examples/units.ts
@@ -15,3 +15,10 @@ export const divide = (a: number, b: number) => {
 // it should return one string combining the two strings.
 // it should throw an error if either of the strings are empty.
 // ensure your function is exported.
+
+export const concatenate = (s1: string, s2:string) => {
+    if(!s1 || !s2) {
+        throw new Error('Empty string found!');
+    }
+    return s1.concat(s2);
+}
